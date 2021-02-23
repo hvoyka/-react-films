8dc41cb2f0f5288987c0be7be1c1b94ad9cd31cf
Lavrentev Stanislav <hvoinii@gmail.com>
2021-02-23 18:02:44 +0600

    FIX: add loading in state, refactoring for bed request

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,12 +1,16 @@
 import { Movie } from "./Movie";
 
 function MoviesList(props) {
-  const { movies } = props;
+  const { movies = [] } = props;
   return (
     <div className="movies__list">
-      {movies.map((movie) => {
-        return <Movie key={movie.imdbID} {...movie} />;
-      })}
+      {movies.length ? (
+        movies.map((movie) => {
+          return <Movie key={movie.imdbID} {...movie} />;
+        })
+      ) : (
+        <h4>Nothing found :(</h4>
+      )}
     </div>
   );
 }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -9,33 +9,34 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 class Main extends React.Component {
   state = {
     movies: [],
+    loading: true,
   };
 
   componentDidMount() {
     fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((response) => response.json())
       .then((data) => {
-        this.setState({ movies: data.Search });
+        this.setState({ movies: data.Search, loading: false });
       });
   }
 
   loadSearchResult = (searchString, filter = "") => {
-    this.setState({ filter });
+    this.setState({ loading: true });
     fetch(
       `http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchString}&type=${filter}`
     )
       .then((response) => response.json())
       .then((data) => {
-        if (data.Response === "True") this.setState({ movies: data.Search });
+        this.setState({ movies: data.Search, loading: false });
       });
   };
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
     return (
       <main className="content container">
         <Search loadSearchResult={this.loadSearchResult} />
-        {movies.length ? <MoviesList movies={movies} /> : <Preloader />}
+        {loading ? <Preloader /> : <MoviesList movies={movies} />}
       </main>
     );
   }
